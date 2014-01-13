aa3fbfbe80c19590e7187e5e5c6d57454d316123
Jamie <jamie@tidepool.org>
2014-01-14 11:18:52 +1300

    change data format based on tests, first basic tests for GroupItemList

--- src/components/GroupItemList.js
+++ src/components/GroupItemList.js
@@ -4,8 +4,53 @@ var React = require('react');
 var GroupItem = require('./GroupItem');
 
 var GroupItemList = React.createClass({
+
+	summaryForMessage: function(message){
+
+	},
+
+	mostRecentMessage: function(messages){
+		if(messages){
+			return messages[0];
+		}
+		return {};
+	},
+
+	niceTime: function(time){
+
+	},
+
+	renderItems: function(){
+
+		var items = this.props.groups.map(function(group, i) {
+
+			var mostRecentMessage = this.mostRecentMessage(group.messages);		
+
+            return (
+            	/* jshint ignore:start */
+                <GroupItem
+                    onClick={this.props.onGroupSelected.bind(this.props, i)}
+                    key={group.groupid} 
+                    name={group.name}
+                    latestNoteSummary={this.summaryForMessage(mostRecentMessage.messagetext)}
+                    when={this.niceTime(mostRecentMessage.timestamp)}/>
+                /* jshint ignore:end */    
+            );
+        }.bind(this));
+
+        return items;
+	},
+
     render: function() {
-        return <div></div>;
+        var items = this.renderItems();
+
+        return (
+        	/* jshint ignore:start */
+            <div className="list-group">
+                {items}
+            </div>
+            /* jshint ignore:end */
+        );
     }
 });
 
--- test/unit/GroupItemList_Test.js
+++ test/unit/GroupItemList_Test.js
@@ -3,14 +3,22 @@ var expect = chai.expect;
 var React = require('react');
 
 var GroupItemList = require('../../build/components/GroupItemList');
+var groups = require('../../demo/data').groups;
 
+var handleGroupSelected = function(props, key){
+    console.log('props: ',props);
+    console.log('key: ',key);
+    return true;
+};
 
 describe('GroupItemList', function() {
     var component, container;
 
+    console.log('data',groups[0]);
+
     beforeEach(function() {
         //we add our component to test into a div and then render it
-        component = GroupItemList();
+        component = GroupItemList({groups:groups,onGroupSelected:handleGroupSelected});
 
         container = document.createElement('div');
         document.documentElement.appendChild(container);
@@ -23,8 +31,16 @@ describe('GroupItemList', function() {
         document.documentElement.removeChild(container);
     });
 
-    it('should exist', function() {
-        expect(component).to.exist;
+    it('should have method to get most recent message based on timestamp', function() {
+        expect(component.mostRecentMessage).to.exist;
+    });
+
+    it('should have method to get a summary of the content of the most recent message', function() {
+        expect(component.summaryForMessage).to.exist;
+    });
+
+    it('should have method to get a nice time for display from the timestamp', function() {
+        expect(component.niceTime).to.exist;
     });
 
 });
